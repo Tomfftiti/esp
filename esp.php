ad43de6ddacb35ce0d8615552a590ae73de696ce
Tomfftiti <117576680+Tomfftiti@users.noreply.github.com>
2022-11-11 20:03:57 +0200

    Update esp.php

--- esp.php
+++ esp.php
@@ -1,11 +1,16 @@
-<?php 
-$varl = $_GET['apples'];
-$var2 = $_GET['oranges'];
-$fileContent = "You have ". $var1." apples, and ".$var2." oranges.\n";
-$fileStatus = file_put_contents('JSC.txt', $fileContent, FILE_APPEND);
+<DOCTYBE html>
+<html>
+ <body>
+ <?php 
+ echo "you are at the right place";
+ $varl = $_GET['apples'];
+ $var2 = $_GET['oranges'];
+ $fileContent = "You have ". $var1." apples, and ".$var2." oranges.\n";
+ $fileStatus = file_put_contents('JSC.txt', $fileContent, FILE_APPEND);
  if($fileStatus != false)
-echo "SUCCESS: data written to file";
-else
-echo "FAIL: could not write to file";
-?>
-
+ echo "SUCCESS: data written to file";
+ else
+ echo "FAIL: could not write to file";
+ ?>
+ </body>
+</html>
