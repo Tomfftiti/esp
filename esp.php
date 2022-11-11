4073414834bd37378cef10e33fe136f0f944357d
Tomfftiti <117576680+Tomfftiti@users.noreply.github.com>
2022-11-11 15:49:25 +0200

    Update esp.php

--- esp.php
+++ esp.php
@@ -1,6 +1,6 @@
 <?php 
-$varl - $_GET['apples'];
-$var2 - $_GET['oranges'];
+$varl = $_GET['apples'];
+$var2 = $_GET['oranges'];
 $fileContent = "You have ". $var1." apples, and ".$var2." oranges.\n";
 $fileStatus = file_put_contents('JSC.txt', $fileContent, FILE_APPEND);
  if($fileStatus != false)
